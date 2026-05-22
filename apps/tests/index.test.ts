da05f11242e350147b4995ad76d3a2ed8453b5a4
Abhishek B R <byteblaster1468@gmail.com>
2026-05-22 10:49:54 +0530

    signup tests worked

--- apps/tests/index.test.ts
+++ apps/tests/index.test.ts
@@ -1,4 +1,47 @@
 import axios, { AxiosError } from "axios";
 import { beforeAll, describe, expect, it } from "bun:test";
 import { BACKEND } from "./config";
+import { password } from "bun";
 
+describe("auth endpoints", () => {
+  const username = "abhishek-" + Math.random();
+
+  it("Signup without password should not work", async () => {
+    try {
+      const response = await axios.post(`${BACKEND}/signup`, {
+        username,
+      });
+
+      expect().fail();
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        expect(e.response?.status).toBe(411);
+      } else {
+        expect().fail();
+      }
+    }
+  });
+
+  it("Signup without username should not work", async () => {
+    try {
+      const response = await axios.post(`${BACKEND}/signup`, {
+        password: "123random",
+      });
+      expect().fail();
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        expect(e.response?.status).toBe(411);
+      } else {
+        expect().fail();
+      }
+    }
+  });
+
+  it("Signup should work on valid inputs", async () => {
+    const response = await axios.post(`${BACKEND}/signup`, {
+      username,
+      password: "123random",
+    });
+    expect(response.data.userId).not.toBe(undefined);
+  });
+});
